b0230c2b7d755428b10a36e11d8e43f68a2a44a2
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2024-05-20 11:28:51 +0530

    Added - button component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  isDisabled = input<boolean>();
+  clicked = output();
+
+  onClick(e: Event) {
+    e.preventDefault();
+    this.clicked.emit();
+  }
+}
