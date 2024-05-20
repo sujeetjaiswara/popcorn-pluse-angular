15300a24abd1c3e129d4cddd28e38db4c056e738
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2024-05-20 12:39:20 +0530

    Completed - withViewTransitions api implimention

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,12 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes
-      // withViewTransitions()
-    ),
+    provideRouter(routes, withViewTransitions()),
     provideClientHydration(),
     provideHttpClient(withFetch()),
   ],
