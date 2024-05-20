38579bf8a5a46e3cd341a1a53a9c3e906a95d231
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2024-05-20 11:30:56 +0530

    Completed - angular image placeholder implimention

--- src/app/components/move-list-item/move-list-item.component.ts
+++ src/app/components/move-list-item/move-list-item.component.ts
@@ -1,14 +1,15 @@
-import { JsonPipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { JsonPipe, NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Movie } from '../../interfaces/movie';
 
 @Component({
   selector: 'app-move-list-item',
   standalone: true,
-  imports: [JsonPipe, RouterLink],
+  imports: [JsonPipe, RouterLink, NgOptimizedImage],
   templateUrl: './move-list-item.component.html',
   styleUrl: './move-list-item.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MoveListItemComponent {
   movie = input<Movie>();
@@ -16,4 +17,8 @@ export class MoveListItemComponent {
   getPoster(posterPath: any) {
     return `https://image.tmdb.org/t/p/w220_and_h330_face${posterPath}`;
   }
+
+  // getRating(vote_average: number) {
+  //   return Math.floor(vote_average);
+  // }
 }
