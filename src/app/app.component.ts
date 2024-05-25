7f4ded54899034441e55c81c6d60cb5235aecb8f
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2024-05-26 00:48:24 +0530

    Added - PWA version detect logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, afterNextRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from './navbar/navbar.component';
 
@@ -11,7 +12,28 @@ import { NavbarComponent } from './navbar/navbar.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  afterNextRender() {
-    initFlowbite();
+  constructor(updates: SwUpdate) {
+    updates.versionUpdates.subscribe((evt: any) => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(
+            `New app version ready for use: ${evt.latestVersion.hash}`
+          );
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(
+            `Failed to install app version '${evt.version.hash}': ${evt.error}`
+          );
+          break;
+      }
+    });
+
+    afterNextRender(() => {
+      initFlowbite();
+    });
   }
 }
