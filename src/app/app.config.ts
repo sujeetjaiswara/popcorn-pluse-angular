29ad8e6ea048a4d45463bb8d5016bacf531e7036
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2024-06-02 21:26:11 +0530

    Updated - App name

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,5 +14,6 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    // provideExperimentalZonelessChangeDetection(),
   ],
 };
