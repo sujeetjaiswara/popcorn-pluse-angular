905f688a0e54c13d0f128e4d6b6c045049c24199
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2024-07-12 01:32:49 +0530

    ♻️Code refector

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -11,9 +11,11 @@ import {
 } from '@angular/core';
 import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 import {RouterLink} from '@angular/router';
+import {finalize} from 'rxjs';
 import {FilterInputComponent, MoveListItemComponent, SearchInputComponent} from '../../components';
+import {DataNotFoundComponent} from '../../components/data-not-found/data-not-found.component';
 import {ButtonComponent, CardPlaceholderComponent, SpinnerComponent} from '../../components/ui';
-import {Category} from '../../models';
+import {Category, Movie} from '../../models';
 import {DataService, MovieService} from '../../services';
 
 @Component({
@@ -27,22 +29,25 @@ import {DataService, MovieService} from '../../services';
     SpinnerComponent,
     ButtonComponent,
     CardPlaceholderComponent,
+    DataNotFoundComponent,
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {
-  protected dataService = inject(DataService);
-  protected movieService = inject(MovieService);
-  private destroy: DestroyRef = inject(DestroyRef);
-  public selectedCategory = signal<string>('popular');
-  public page = signal<number>(1);
-  public searchTerm = signal<string>('');
-  public isLoading = signal<boolean>(false);
-  public isLoadingMore = signal<boolean>(false);
+  #dataService = inject(DataService);
+  #movieService = inject(MovieService);
+  #destroy: DestroyRef = inject(DestroyRef);
+  #selectedCategory = signal<string>('popular');
+  #page = signal<number>(1);
+  #searchTerm = signal<string>('');
 
-  public categories: WritableSignal<Category[]> = signal([
+  movies: Movie[] = [];
+  isLoading = false;
+  isLoadingMore = false;
+
+  categories: WritableSignal<Category[]> = signal([
     {value: 'popular', name: 'Popular'},
     {value: 'top_rated', name: 'Top Rated'},
     {value: 'now_playing', name: 'Now Playing'},
@@ -51,55 +56,80 @@ export class HomePageComponent {
 
   constructor(private cd: ChangeDetectorRef) {
     // TODO: Refector this code
-    if (this.movieService.movies().length === 0) {
-      this.isLoading.set(true);
+    if (this.#movieService.movies().length === 0) {
+      this.#movieService.setIsLoading(true);
     }
 
     afterNextRender(() => {
       console.log('🚀afterNextRender() called.');
-      if (this.movieService.movies().length === 0) {
-        this.getMovieByCategory();
+
+      if (this.#movieService.movies().length > 0) {
+        this.movies = this.#movieService.movies();
+        this.cd.markForCheck();
+        return;
       }
+
+      this.getMovieByCategory();
     });
 
     effect(() => {
-      // console.log('movies:[]', this.movieService.movies());
+      console.log('🚦isLoading effect() called.');
+      this.isLoading = this.#movieService.isLoading();
+      this.cd.markForCheck();
+    });
+
+    effect(() => {
+      console.log('🚦isLoadingMore effect() called.');
+      this.isLoadingMore = this.#movieService.isLoadingMore();
+      this.cd.markForCheck();
+    });
+
+    effect(() => {
+      console.log('🚦movies effect() called.');
+      this.movies = this.#movieService.movies();
+      this.cd.markForCheck();
     });
   }
 
   getMovieByCategory() {
-    this.isLoading.set(true);
-    this.dataService
-      .getMovieByCategory(this.selectedCategory(), this.page())
-      .pipe(takeUntilDestroyed(this.destroy))
+    this.#movieService.setIsLoading(true);
+    this.#dataService
+      .getMovieByCategory(this.#selectedCategory(), this.#page())
+      .pipe(
+        finalize(() => {
+          this.#movieService.setIsLoadingMore(false);
+          this.#movieService.setIsLoading(false);
+        }),
+        takeUntilDestroyed(this.#destroy),
+      )
       .subscribe({
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        next: (data: any) => this.movieService.setMovies(data.results),
+        next: (data: any) => this.#movieService.setMovies(data.results),
         error: (err) => console.error(err),
-        complete: () => {
-          this.isLoadingMore.set(false);
-          this.isLoading.set(false);
-        },
+        complete: () => console.info('✅getMovieByCategory() complete'),
       });
   }
 
   filterMovies(value: string) {
-    this.selectedCategory.set(value);
-    this.movieService.movies.set([]);
+    this.#selectedCategory.set(value);
+    this.#movieService.resetMovieData();
     this.getMovieByCategory();
   }
 
   searchMovie() {
-    this.page.set(1);
-    if (this.searchTerm) {
-      this.dataService
-        .searchMovie(this.searchTerm(), this.page())
-        .pipe(takeUntilDestroyed(this.destroy))
+    this.#page.set(1);
+    if (this.#searchTerm()) {
+      this.#dataService
+        .searchMovie(this.#searchTerm(), this.#page())
+        .pipe(takeUntilDestroyed(this.#destroy))
         .subscribe({
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          next: (data: any) => this.movieService.movies.set(data.results),
+          next: (data: any) => {
+            this.#movieService.resetMovieData();
+            this.#movieService.setMovies(data.results);
+          },
           error: (err) => console.error('Error:', err),
-          complete: () => console.info('complete'),
+          complete: () => console.info('searchMovie() complete'),
         });
     } else {
       this.getMovieByCategory();
@@ -107,8 +137,8 @@ export class HomePageComponent {
   }
 
   loadMore() {
-    this.page.set(this.page() + 1);
-    this.isLoadingMore.set(true);
+    this.#page.set(this.#page() + 1);
+    this.#movieService.setIsLoadingMore(true);
     this.getMovieByCategory();
   }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -5,25 +5,45 @@ import {Movie} from '../models';
   providedIn: 'root',
 })
 export class MovieService {
-  public movies = signal<Movie[]>([]);
-  public selectedMovie = signal<Movie | null>(null);
-  public similar = signal<Movie[]>([]);
+  #movies = signal<Movie[]>([]);
+  #selectedMovie = signal<Movie | null>(null);
+  #similar = signal<Movie[]>([]);
+  #isLoading = signal<boolean>(false);
+  #isLoadingMore = signal<boolean>(false);
+
+  // Getters
+  readonly movies = this.#movies.asReadonly();
+  readonly selectedMovie = this.#selectedMovie.asReadonly();
+  readonly isLoading = this.#isLoading.asReadonly();
+  readonly isLoadingMore = this.#isLoadingMore.asReadonly();
+
+  setIsLoading(value: boolean) {
+    this.#isLoading.set(value);
+  }
+
+  setIsLoadingMore(value: boolean) {
+    this.#isLoadingMore.set(value);
+  }
 
   setMovies(data: Movie[]) {
     // Create a Set to keep track of unique movie IDs
-    const uniqueMovieIds = new Set(this.movies().map((movie: Movie) => movie.id));
+    const uniqueMovieIds = new Set(this.#movies().map((movie: Movie) => movie.id));
 
     // Filter out duplicates by checking if the movie ID is already in the Set
     const uniqueMovies = data.filter((movie: Movie) => !uniqueMovieIds.has(movie.id));
 
-    this.movies.update((items) => [...items, ...uniqueMovies]);
+    this.#movies.update((items) => [...items, ...uniqueMovies]);
   }
 
   setSelectedMovie(values: Movie) {
-    this.selectedMovie.set(values);
+    this.#selectedMovie.set(values);
   }
 
   setSimilarMovies(data: Movie[]) {
-    this.similar.set([...data]);
+    this.#similar.set([...data]);
+  }
+
+  resetMovieData() {
+    this.#movies.set([]);
   }
 }
