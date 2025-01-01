b89251a79bb90e78b811b9b4f55ffe12bb8c17da
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-01-01 20:43:33 +0530

    Optimized - flowbite initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, afterNextRender, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
-import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FlowbiteService } from './services/flowbite.service';
 
 @Component({
   selector: 'app-root',
@@ -12,27 +12,33 @@ import { NavbarComponent } from './navbar/navbar.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  #flowbiteService = inject(FlowbiteService);
+
   constructor() {
     const updates = inject(SwUpdate);
+    updates.versionUpdates.subscribe({
+      next: (event: any) => {
+        switch (event.type) {
+          case 'VERSION_DETECTED':
+            console.log(`Downloading new app version: ${event.version.hash}`);
+            break;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    updates.versionUpdates.subscribe((evt: any) => {
-      switch (evt.type) {
-        case 'VERSION_DETECTED':
-          console.log(`Downloading new app version: ${evt.version.hash}`);
-          break;
-        case 'VERSION_READY':
-          console.log(`Current app version: ${evt.currentVersion.hash}`);
-          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
-          break;
-        case 'VERSION_INSTALLATION_FAILED':
-          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
-          break;
-      }
+          case 'VERSION_READY':
+            console.log(`Current app version: ${event.currentVersion.hash}`);
+            console.log(`New app version ready for use: ${event.latestVersion.hash}`);
+            break;
+
+          case 'VERSION_INSTALLATION_FAILED':
+            console.log(`Failed to install app version '${event.version.hash}': ${event.error}`);
+            break;
+        }
+      },
     });
+  }
 
-    afterNextRender(() => {
-      initFlowbite();
+  ngOnInit(): void {
+    this.#flowbiteService.loadFlowbite((flowbite) => {
+      console.log('Flowbite loaded', flowbite);
     });
   }
 }
