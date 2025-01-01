18949febd774264358c8b629ec0fdea1686ec574
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-01-01 20:44:16 +0530

    Fixed - layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js", // add this line
+    './src/**/*.{html,ts}',
+    './node_modules/flowbite/**/*.js', // add this line
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {},
   },
   plugins: [
-    require("flowbite/plugin"), // add this line
+    require('flowbite/plugin'), // add this line
   ],
 };
